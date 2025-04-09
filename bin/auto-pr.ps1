7b5520955a068367a71381f7729bad0b6b4f7da4
ParkSnoopy <117149837+ParkSnoopy@users.noreply.github.com>
2025-04-09 17:07:25 +0900

    5. Replace the placeholder repository string

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "ParkSnoopy/scoop:main"
+    [String]$upstream = "ParkSnoopy/dev-scoop:main"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
